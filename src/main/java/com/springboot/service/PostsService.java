c43f60d3620552d5e53a243a351ca393750c1992
CHOI HyoJun <cjfgh2449@gmail.com>
2024-03-08 08:14:39 +0900

    등록/수정/조회 화면 구성 및 작동 확인

--- src/main/java/com/springboot/service/PostsService.java
+++ src/main/java/com/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.springboot.service;
 
 import com.springboot.domain.posts.Posts;
 import com.springboot.domain.posts.PostsRepository;
+import com.springboot.web.dto.PostsListResponseDto;
 import com.springboot.web.dto.PostsResponseDto;
 import com.springboot.web.dto.PostsSaveRequestDto;
 import com.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성.
 @Service
@@ -34,5 +38,24 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+     // @Transactional 어노테이션을 통해 개발자는 메소드는 실행을 트랜잭션 경계로 정의 가능.
+    // readOnly 옵션을 true로 주면 트랜잭션 범위는 유지하되 조회 기능만 유지하게 되어 조회속도 상승
+    // 데이터의 변경의 없는 서비스 메소드에서 사용 추천
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        // .map(posts -> new PostsListsResponseDto(posts)) 와 같음
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
