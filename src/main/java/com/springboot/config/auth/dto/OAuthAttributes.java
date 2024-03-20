b288357e7a39749c0cb46cfa9299ada3f57e92f7
CHOI HyoJun <cjfgh2449@gmail.com>
2024-03-21 01:08:49 +0900

    gradle test 빌드 오류 수정

--- src/main/java/com/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/config/auth/dto/OAuthAttributes.java
@@ -60,10 +60,8 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
-
-
 
 }
