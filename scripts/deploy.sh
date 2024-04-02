f96fe3fd33edf87c7c3cc1845c87a28a8cc262dd
CHOI HyoJun <cjfgh2449@gmail.com>
2024-04-02 09:34:01 +0900

    CI/CD 완료 배포 확인

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,11 +20,11 @@ else
         kill -15 $CURRENT_PID
         sleep 5
 fi
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
@@ -32,4 +32,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties -Dspring.profiles.active=real $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
